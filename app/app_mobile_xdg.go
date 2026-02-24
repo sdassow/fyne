9beb8fbcb566dc8d837648a5ebc0967e39c6c438
Max Schillinger <maxschillinger@web.de>
2026-02-24 11:02:02 +0100

    Support OpenURL in mobile-xdg on macOS

--- app/app_mobile_xdg.go
+++ app/app_mobile_xdg.go
@@ -6,14 +6,21 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"runtime"
 
 	"fyne.io/fyne/v2"
 )
 
 func (a *fyneApp) OpenURL(url *url.URL) error {
-	cmd := exec.Command("xdg-open", url.String())
-	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
-	return cmd.Start()
+	if runtime.GOOS == "darwin" {
+		cmd := exec.Command("open", url.String())
+		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+		return cmd.Run()
+	} else {
+		cmd := exec.Command("xdg-open", url.String())
+		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+		return cmd.Start()
+	}
 }
 
 func (a *fyneApp) SendNotification(_ *fyne.Notification) {
