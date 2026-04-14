da10c96e5f8d5ad32cb362da7cd49400378a163f
Andy Williams <andy@andy.xyz>
2026-04-14 20:20:51 +0100

    Fix doc and import order to solve warnings

--- driver/desktop/driver.go
+++ driver/desktop/driver.go
@@ -5,10 +5,10 @@ import "fyne.io/fyne/v2"
 
 // Driver represents the extended capabilities of a desktop driver
 type Driver interface {
-	// Create a new borderless window that is centered on screen
+	// CreateSplashWindow creates a new borderless window that is centered on screen
 	CreateSplashWindow() fyne.Window
 
-	// Gets the set of key modifiers that are currently active
+	// CurrentKeyModifiers returns the set of key modifiers that are currently active
 	//
 	// Since: 2.4
 	CurrentKeyModifiers() fyne.KeyModifier
