e617ed76dbd7b1af96fd644f670cb43eda4f04eb
Tilo Prütz <tilo@pruetz.net>
2026-06-08 09:01:22 +0200

    [widget] improve entry_cursor_anim_test alpha assertions
    `assert.Equal` has a much more meaningful message than `assert.True`.

--- widget/entry_cursor_anim_test.go
+++ widget/entry_cursor_anim_test.go
@@ -19,10 +19,9 @@ func TestEntryCursorAnim(t *testing.T) {
 	r, g, b, _ := col.ToNRGBA(cursorOpaque)
 	cursorDim := color.NRGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: 0x16}
 
-	alphaEquals := func(color1, color2 color.Color) bool {
-		_, _, _, a1 := col.ToNRGBA(color1)
-		_, _, _, a2 := col.ToNRGBA(color2)
-		return uint8(a1>>8) == uint8(a2>>8) // only check 8bit colour channels
+	alpha := func(c color.Color) uint8 {
+		_, _, _, a := col.ToNRGBA(c)
+		return uint8(a >> 8) // only check 8bit colour channels
 	}
 
 	cursor := canvas.NewRectangle(color.Black)
@@ -30,17 +29,17 @@ func TestEntryCursorAnim(t *testing.T) {
 
 	a.start()
 	a.anim.Tick(0.0)
-	assert.True(t, alphaEquals(cursorDim, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorDim), alpha(a.cursor.FillColor))
 	a.anim.Tick(1.0)
-	assert.True(t, alphaEquals(cursorOpaque, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorOpaque), alpha(a.cursor.FillColor))
 
 	a.interrupt()
 	a.anim.Tick(0.0)
-	assert.True(t, alphaEquals(cursorOpaque, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorOpaque), alpha(a.cursor.FillColor))
 	a.anim.Tick(0.5)
-	assert.True(t, alphaEquals(cursorOpaque, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorOpaque), alpha(a.cursor.FillColor))
 	a.anim.Tick(1.0)
-	assert.True(t, alphaEquals(cursorOpaque, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorOpaque), alpha(a.cursor.FillColor))
 
 	timeNow = func() time.Time {
 		return time.Now().Add(cursorInterruptTime)
@@ -50,14 +49,14 @@ func TestEntryCursorAnim(t *testing.T) {
 	runtime.Gosched()
 	time.Sleep(10 * time.Millisecond) // ensure go routine for restart animation is executed
 	a.anim.Tick(0.0)
-	assert.True(t, alphaEquals(cursorOpaque, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorOpaque), alpha(a.cursor.FillColor))
 	a.anim.Tick(1.0)
-	assert.True(t, alphaEquals(cursorDim, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorDim), alpha(a.cursor.FillColor))
 
 	timeNow = time.Now
 	a.interrupt()
 	a.anim.Tick(0.0)
-	assert.True(t, alphaEquals(cursorOpaque, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorOpaque), alpha(a.cursor.FillColor))
 
 	timeNow = func() time.Time {
 		return time.Now().Add(cursorInterruptTime)
@@ -66,9 +65,9 @@ func TestEntryCursorAnim(t *testing.T) {
 	runtime.Gosched()
 	time.Sleep(10 * time.Millisecond) // ensure go routine for restart animation is executed
 	a.anim.Tick(0.0)
-	assert.True(t, alphaEquals(cursorOpaque, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorOpaque), alpha(a.cursor.FillColor))
 	a.anim.Tick(1.0)
-	assert.True(t, alphaEquals(cursorDim, a.cursor.FillColor))
+	assert.Equal(t, alpha(cursorDim), alpha(a.cursor.FillColor))
 
 	a.stop()
 	assert.Nil(t, a.anim)
